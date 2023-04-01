ae5cf923c50e731902dd3e7ae0f834086eabe249
beenbin <77373566+showme0241@users.noreply.github.com>
2023-04-01 14:38:46 +0900

    :lipstick: Design : 공용 UI 파일 추가

---
+++ src/Components/Layouts/Footer/Footer.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+function Footer() {
+	return <S.FooterWrapper></S.FooterWrapper>
+}
+
+export default Footer
+
+const FooterWrapper = styled.footer``
+
+const S = {
+	FooterWrapper,
+}
---
+++ src/Components/Layouts/Header/Header.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+function Header() {
+	return <S.HeaderWrapper></S.HeaderWrapper>
+}
+
+export default Header
+
+const HeaderWrapper = styled.header``
+
+const S = {
+	HeaderWrapper,
+}

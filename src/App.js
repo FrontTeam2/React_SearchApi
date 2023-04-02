5309eef51c8168c4c66216d1abc65dfea7e9c772
beenbin <77373566+showme0241@users.noreply.github.com>
2023-04-02 13:57:11 +0900

    :art: Feat : App.js 전역 관리할 수 있도록 구조 변경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import theme from './Styles/theme'
+import GlobalStyles from './Styles/global'
+import { RouterProvider } from 'react-router-dom'
+import router from './Routes/routing'
+
 function App() {
-  return <div>:)</div>;
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<RouterProvider router={router} />
+		</ThemeProvider>
+	)
 }
 
-export default App;
+export default App
